e4cdd6533de7d431df3634da14a798851fa04ea5
padifalas <padifalas@outlook.com>
2024-09-08 22:49:43 +0200

    glue stops player health from draining
    * NEED to restart drainage agin

--- Assets/GameScripts/GlueBottle.cs
+++ Assets/GameScripts/GlueBottle.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+/* 
 public class GlueBottle : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
@@ -12,5 +14,33 @@ public class GlueBottle : MonoBehaviour
 
             Destroy (gameObject);
         }
+    }
+}
+*/
+
+public class GlueBottle : MonoBehaviour
+{
+    public float stunDuration = 5f; // Duration of the health glue in seconds
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            StuffingEffects stuffingEffects = other.GetComponent<StuffingEffects>();
+
+            if (stuffingEffects != null)
+            {
+                stuffingEffects.StopStuffingEffect();
+                StartCoroutine(RestartStuffingEffectAfterDelay(stuffingEffects, stunDuration));
+            }
+
+            Destroy(gameObject);
+        }
+    }
+
+    private IEnumerator RestartStuffingEffectAfterDelay(StuffingEffects stuffingEffects, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        stuffingEffects.HandleStuffingEffects(); // start health drain again after glue finishes
     }
 }
--- Assets/GameScripts/StuffingEffects.cs
+++ Assets/GameScripts/StuffingEffects.cs
@@ -7,6 +7,66 @@ public class StuffingEffects : MonoBehaviour
     private PlayerStats playerStats;
     public GameObject GameObject;
 
+    private Coroutine stuffingDamageCoroutine;
+
+    public void Start()
+    {
+        playerStats = GetComponent<PlayerStats>();
+    }
+
+    public void HandleStuffingEffects()
+    {
+        if (stuffingDamageCoroutine == null)
+        {
+            stuffingDamageCoroutine = StartCoroutine(StuffingDamage());
+        }
+    }
+
+    private IEnumerator StuffingDamage()
+    {
+        float amount = 2f;
+        float interval = 1f;
+
+        while (playerStats.GetCurrentHealth() > 0)
+        {
+            playerStats.DecreaseHealth(amount);
+            playerStats.healthBar.SetSlider(playerStats.GetCurrentHealth());
+
+            yield return new WaitForSeconds(interval);
+        }
+    }
+
+    public void StopStuffingEffect()
+    {
+        if (stuffingDamageCoroutine != null)
+        {
+            StopCoroutine(stuffingDamageCoroutine);
+            stuffingDamageCoroutine = null;
+            StartCoroutine(Stop());
+            Debug.Log("Stuffing effect stopped");
+        }
+    }
+
+    IEnumerator Stop()
+    {
+        GameObject.SetActive(true);
+
+        yield return new WaitForSeconds(10);
+
+        GameObject.SetActive(false);
+    }
+}
+
+
+
+
+
+/*
+public class StuffingEffects : MonoBehaviour
+{
+    private PlayerStats playerStats;
+    public GameObject GameObject;
+
     public void Start()
     {
         playerStats = GetComponent<PlayerStats>();
@@ -46,4 +106,6 @@ public class StuffingEffects : MonoBehaviour
         GameObject.SetActive(false);
     }
 }
+*/
+
 
